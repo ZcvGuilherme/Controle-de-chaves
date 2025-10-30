c19058c873584870075c404a169ecdebfed48821
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-10-30 16:07:52 -0300

    começo da criação dos testes

---
+++ guarita/tests/models/test_busca.py
@@ -0,0 +1,75 @@
+from django.test import TestCase
+from guarita.models import Pessoa, Chave, Historico, ChaveStatus
+from django.db.models.query import QuerySet
+class PessoaBuscaTests(TestCase):
+    def setUp(self):
+        self.pessoa1 = Pessoa.objects.create(
+            matricula=1,
+            nome="João",
+            cargo= "Professor"
+        )
+        self.pessoa2 = Pessoa.objects.create(
+            matricula=2,
+            nome="Maria",
+            cargo= "Técnico"
+        )
+        self.pessoa3 = Pessoa.objects.create(
+            matricula=3,
+            nome="Joana",
+            cargo="Bibliotecário"
+        )
+        self.pessoa4 = Pessoa.objects.create(
+            matricula=4,
+            nome="Jorge",
+            cargo="Professor"
+        )
+
+    def test_busca_por_id(self):
+        busca = Pessoa.search_by_matricula(self.pessoa1.matricula)
+
+        self.assertIsNotNone(busca)
+        self.assertEqual(busca.nome, self.pessoa1.nome)
+        self.assertEqual(busca.cargo, self.pessoa1.cargo)
+
+
+    def test_busca_por_nome(self):
+        busca = Pessoa.search_by_nome(self.pessoa1.nome)
+
+        self.assertIsNotNone(busca)
+        self.assertEqual(busca.nome, self.pessoa1.nome)
+    
+    def test_busca_por_cargo(self):
+         busca = Pessoa.search_by_cargo("Professor")
+
+         self.assertIsNotNone(busca)
+        
+
+    #métodos de busca:
+    #receber uma QuerySet
+    #critérios: 
+
+  
+    #buscar por cargo
+    #busca parcial
+    #buscar todos
+   
+
+class ChaveBuscaTests(TestCase):
+    #Atributos:
+    #id
+    #nome
+
+    #métodos de busca:
+    #receber uma Query
+    pass
+class HistoricoBuscaTests(TestCase):
+    #Atributos:
+    #id_historico
+    #nome
+
+    #métodos de busca:
+    #receber o objeto Chave inteiro
+    pass
+
+class ChaveStatusBuscaTests(TestCase):
+    pass
---
+++ guarita/tests/models/test_update.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from guarita.models import Pessoa, Chave, Historico, ChaveStatus
+
+
+#não é prioridade agora
+class PessoaUpdateTests(TestCase):
+   pass
+
+class ChaveUpdateTests(TestCase):
+    pass
+class HistoricoUpdateTests(TestCase):
+    pass
+
+class ChaveStatusUpdateTests(TestCase):
+    pass
