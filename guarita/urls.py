98712dc0f74e26ca2f9841c7cb97675980953d50
Waldeney Vieira <great1one22@gmail.com>
2025-10-31 18:56:54 -0300

    Criação das paginas Registrar e devolver chave

--- guarita/urls.py
+++ guarita/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.chaves, name='chaves'),
+    path('registrar/', views.registrar_chave, name='registrar_chave'),
+    path('devolver/', views.devolver_chave, name='devolver_chave'),
 ]
--- guarita/views.py
+++ guarita/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def chaves(request):
     return render(request, 'chaves.html')
+
+def registrar_chave(request):
+    return render(request, 'registrar_chave.html')
+
+def devolver_chave(request):
+    return render(request, 'devolver_chave.html')
