54c78b7bf80c9acf9e64a25295dd7a47bc39059d
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-11-01 21:48:28 -0300

    configuracao feita

--- chaves/settings.py
+++ chaves/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chaves'
+    'chaves',
+    'guarita'
 ]
 
 MIDDLEWARE = [
@@ -122,8 +123,5 @@ STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
