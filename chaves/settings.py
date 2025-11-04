89367938dbbfb2f8492e53d80d7bf821826a8e7e
Carvalhop2 <wellingtonp2.oc@gmail.com>
2025-11-04 19:18:49 -0300

    conserto css completo

--- chaves/settings.py
+++ chaves/settings.py
@@ -105,11 +105,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'templates/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 STATIC_ROOT = os.path.join('static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'templates')
-MEDIA_URL = '/templates/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
