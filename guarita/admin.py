06701e52595c698f79f52d9c922f9115276bbb6e
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-11-13 10:27:44 -0300

    mudança minima no DB

--- guarita/admin.py
+++ guarita/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Chave, ChaveStatus, Historico, Pessoa
 
-# Register your models here.
+@admin.register(Chave)
+class ChaveAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'itemBusca')
+
+@admin.register(ChaveStatus)
+class ChaveStatus(admin.ModelAdmin):
+    list_display = ('chave', 'pessoa', 'status_code', 'checkin')
+
+@admin.register(Historico)
+class HistoricoAdmin(admin.ModelAdmin):
+    list_display = ('id_historico','chave', 'pessoa', 'horario')
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('matricula','itemBusca', 'nome', 'cargo')
+
