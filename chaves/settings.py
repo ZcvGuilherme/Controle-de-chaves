b18564d8384d868053530c3faa8fff56f275a288
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-11-25 20:27:29 -0300

    funcionando

--- chaves/settings.py
+++ chaves/settings.py
@@ -115,6 +115,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/login/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'status_chaves'
--- guarita/views.py
+++ guarita/views.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
-from .models import Chave
-
-
 
 @login_required
 def status_chave(request):
