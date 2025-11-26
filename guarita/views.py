920d04e76661efdcad3a3e393a7eda2179b1748b
Carvalhop2 <wellingtonp2.oc@gmail.com>
2025-11-26 20:20:40 -0300

    botao sair configurado para deslograr

--- guarita/views.py
+++ guarita/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-
+from django.views.decorators.cache import never_cache
 from django.shortcuts import render
 
+@never_cache
 @login_required
 def status_chave(request):
     return render(request, 'status_chaves.html')
