41c76b99b6bae839a62688d7d8cfbe1b9f7b89c8
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-12-02 00:18:05 -0300

    signals não funciona e admin atualizado

--- guarita/admin.py
+++ guarita/admin.py
@@ -7,12 +7,15 @@ class ChaveAdmin(admin.ModelAdmin):
     list_display = ('id','nome', 'itemBusca')
     list_display_links = ('id', 'nome')
 @admin.register(ChaveStatus)
-class ChaveStatus(admin.ModelAdmin):
+class ChaveStatusAdmin(admin.ModelAdmin):
     list_display = ('chave', 'pessoa', 'status_code', 'checkin')
     list_display_links = ('chave', 'pessoa')
+
+
+    
 @admin.register(Historico)
 class HistoricoAdmin(admin.ModelAdmin):
-    list_display = ('id_historico','chave', 'pessoa', 'horario')
+    list_display = ('id_historico','acao','chave', 'pessoa', 'horario')
     list_display_links = ('id_historico', 'chave')
 @admin.register(Pessoa)
 class PessoaAdmin(admin.ModelAdmin):
--- guarita/signals.py
+++ guarita/signals.py
@@ -13,38 +13,3 @@ def gerar_itemBusca(sender, instance, created, **kwargs):
     if created:
         instance.itemBusca = f"Chave {instance.id} - {instance.nome}"
         instance.save(update_fields=["itemBusca"])
-
-@receiver(pre_save, sender=ChaveStatus)
-def registrar_historico_auto(sender, instance, **kwargs):
-    """
-    Receiver que registra o histórico automaticamente.
-    
-    Verifica primeiramente se é uma criação de um status novo
-
-    Tenta  
-    """
-    if not instance.pk:
-        # É criação, não registrar histórico
-        return
-    
-    try:
-        #Anterior é a versão do banco de dados, ou seja, a versão que ainda não foi atualizada
-        anterior = ChaveStatus.objects.get(pk=instance.pk)
-    except ChaveStatus.DoesNotExist:
-        return
-
-    # Se pessoa mudou → RETIRADA ou DEVOLUCAO
-    if anterior.pessoa != instance.pessoa:
-
-        if instance.pessoa is None:
-            acao = "DEVOLUCAO"
-            pessoa = anterior.pessoa
-        else:
-            acao = "RETIRADA"
-            pessoa = instance.pessoa
-
-        Historico.registrar_acesso(
-            acao=acao,
-            pessoa=pessoa,
-            chave=instance.chave,
-        )
