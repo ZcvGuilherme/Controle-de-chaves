77942b011aa1dc6246467db5c6dfe6c9e065b224
ZcvGuilherme <guisousasilvanota10@gmail.com>
2026-03-11 23:30:12 -0300

    segunda opcao q faz a mesma coisa

--- guarita/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import PermissionDenied
-from .models import Pessoa
-
-def get_pessoa_from_user(user):
-    try:
-        return user.pessoa
-    except Pessoa.DoesNotExist:
-        raise PermissionDenied("Usuário não vinculado a uma pessoa.")
